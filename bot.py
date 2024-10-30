bd3550e682814645fd75b0565dea7a8715ea1cf2
naruto0927 <pritbhandari8@gmail.com>
2024-10-30 19:40:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
+
 from datetime import datetime
 from pytz import timezone
-from pyrogram import Client, __version__
+from pyrogram import Client, version
 from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
 import os
 
 class Bot(Client):
-    def __init__(self):
-        super().__init__(
+    def init(self):
+        super().init(
             name="renamer",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
@@ -24,21 +25,24 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
+        self.uptime = datetime.fromisoformat(Config.BOT_UPTIME)     
         if Config.WEB_SUPPORT:
             app = web.AppRunner(web.Application(client_max_size=30000000))
             await app.setup()
             await web.TCPSite(app, "0.0.0.0", 8080).start()
             
         print(f"\033[1;96m @{me.username} Sᴛᴀʀᴛᴇᴅ......⚡️⚡️⚡️\033[0m")
-        try: [await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**") for id in Config.ADMIN]                              
+        try: 
+            for id in Config.ADMIN:
+                await self.send_message(id, f"{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")                              
         except: pass
         if Config.LOG_CHANNEL:
             try:
                 curr = datetime.now(timezone("Asia/Kolkata"))
                 date = curr.strftime('%d %B, %Y')
                 time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
+                chat = await self.get_chat(Config.LOG_CHANNEL)
+                await self.send_message(chat.id, f"__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : {date}\n⏰ Tɪᴍᴇ : {time}\n🌐 Tɪᴍᴇᴢᴏɴᴇ : Asia/Kolkata\n\n🉐 Vᴇʀsɪᴏɴ : v{__version__} (Layer {layer})")                                
             except:
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
